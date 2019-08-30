2b9bfdd0ba07f62addcf05b99dda9f3862c6b03d
Ashish Kapoor <swiftobjc@gmail.com>
2019-08-30 20:37:32 +0530

    Add tests. "To TDD or Not To TDD".

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'foo_pages/bar'
-  get 'foo_pages/bas'
   get 'static_pages/home'
   get 'static_pages/help'
+  get  'static_pages/about'
   root 'application#hello'
 end
